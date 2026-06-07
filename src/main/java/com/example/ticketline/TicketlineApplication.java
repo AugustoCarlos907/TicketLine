ca4314ec0b743c939511125579519102bf6f9deb
Augusto Carlos <augustoaccarlos@gmail.com>
2026-06-07 11:45:28 +0100

    first commit

---
+++ src/main/java/com/example/ticketline/TicketlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ticketline/TicketlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
